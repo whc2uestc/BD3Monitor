ededeb6c6709777ac409a34567bd2836bce94a99
HC.Wang <171094685@qq.com>
2015-10-25 11:59:15 +0800

    the head file of b2 initial.

---
+++ b2/inc/init.h
@@ -0,0 +1,14 @@
+/*
+ * init.h
+ * Date   : 11-14-2014
+ * Author : HouBiao
+*/
+
+#ifndef B2INIT_H_
+#define B2INIT_H_
+#include <stdio.h>
+#include"type.h"
+
+extern void B2ChnlInit(B2ChnlPrms *chnlInst);				// B2≥ı ºªØ
+
+#endif
