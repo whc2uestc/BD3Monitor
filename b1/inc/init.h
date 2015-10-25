4575c066c475d33bdda84a099dcd46ffc879ad52
HC.Wang <171094685@qq.com>
2015-10-25 11:00:22 +0800

    the head file of b1 initial.

---
+++ b1/inc/init.h
@@ -0,0 +1,13 @@
+/*
+ * init.h
+ * Date   : 11-14-2014
+ * Author : WangHanCheng
+*/
+#ifndef B1INIT_H_
+#define B1INIT_H_
+#include <stdio.h>
+#include"type.h"
+
+extern void B1ChnlInit(B1ChnlPrms *chnlInst);
+
+#endif
