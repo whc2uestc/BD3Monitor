176719c39aba0d7758b7bad9d57942d145b7d55f
HC.Wang <171094685@qq.com>
2016-05-17 10:37:46 +0800

    The source file of b2  tracking.

---
+++ c/b2/inc/trk.h
@@ -0,0 +1,14 @@
+/*
+ * trk.h
+ * Date   : 11-14-2014
+ * Author : HouBiao
+*/
+
+#ifndef B2TRK_H_
+#define B2TRK_H_
+#include <stdio.h>
+#include"type.h"
+
+extern void B2Trk(B2ChnlPrms *chnlInst,char data);				// B2¸ú×Ù
+
+#endif
