bb371b510bdfcf7ceba099b3daec06ce6d313ad8
HC.Wang <171094685@qq.com>
2016-05-17 10:34:55 +0800

    The head file of b1  tracking.

---
+++ c/b1/inc/trk.h
@@ -0,0 +1,14 @@
+/*
+ * trk.h
+ * Date   : 11-14-2014
+ * Author : WangHanCheng
+*/
+
+#ifndef B1TRK_H_
+#define B1TRK_H_
+#include <stdio.h>
+#include"type.h"
+
+extern void B1TrkASPT(B1ChnlPrms *chnlInst,char data);
+
+#endif
